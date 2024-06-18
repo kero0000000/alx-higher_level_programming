703de1d3b2e1d571715b64d075c92e4ae13d3dd6
“kero0000000” <kerorizk27@gmail.com>
2024-06-18 16:37:39 -0700

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/1.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let name='Gabriel'; // variable decl
+let Name = {}; // object declarion
+//any
+const name = ['Gabriel',123, '56'];
+//classes and inheritance
+class Rectangle{
+	constructtor(height, width){
+      this
+		super()
+}
+}
+//inheritance
+class square extends Rectangle
