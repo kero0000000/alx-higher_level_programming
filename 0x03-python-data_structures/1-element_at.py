70a6c57907223bfac78c4e390c713a9672fe40a9
keroless rizkalla <kerorizk27@gmail.com>
2024-09-01 09:54:52 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,4 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
          return (none)
-    if idx > len(my_list) - 1
+    if idx > len(my_list) - 1:
+         return (None)
+    return my_list[idex]
