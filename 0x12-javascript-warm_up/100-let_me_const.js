d9e5ba705b2baa7e560639ffa2ac9e6886153f4b
keroless rizkalla <kerorizk27@gmail.com>
2024-09-28 02:04:56 +0000

    100-let_me_const.js

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let myVar = 0; // Initial value
+console.log("Before modification:", myVar); // Log initial value
+
+// Modify the value of myVar
+myVar = 333;
+
+console.log("After modification:", myVar); // Log modified value
